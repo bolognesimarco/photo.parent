d0038867d45861b7c9ecbafb74fd0e6fa37941d1
miogituser <bolognesi.marco@gmail.com>
2015-01-31 15:06:49 +0100

    Cambio di struttura utenti

--- photo.model/src/main/java/org/bolo/photo/model/Utente.java
+++ photo.model/src/main/java/org/bolo/photo/model/Utente.java
@@ -1,17 +1,14 @@
 package org.bolo.photo.model;
 
-import javax.persistence.DiscriminatorColumn;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.Inheritance;
-import javax.persistence.InheritanceType;
 
 @Entity
-@Inheritance(strategy=InheritanceType.SINGLE_TABLE)
-@DiscriminatorColumn(name="Discriminator")
-public abstract class Utente {
+public class Utente {
 	@Id
 	@GeneratedValue
 	private int id;
+	
+	
 }

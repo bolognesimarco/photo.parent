4f3cf2a82652d26ea56e93de4ffa458bb77f8b3a
miogituser <bolognesi.marco@gmail.com>
2015-01-31 15:16:44 +0100

    Tanto per..

--- photo.model/src/main/java/org/bolo/photo/model/Utente.java
+++ photo.model/src/main/java/org/bolo/photo/model/Utente.java
@@ -3,12 +3,34 @@ package org.bolo.photo.model;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Utente {
 	@Id
 	@GeneratedValue
 	private int id;
+	
+	@ManyToOne(optional=false)
+	@JoinColumn(name="tipoUtente", nullable=false, updatable=true)
+	private TipoUtente tipoUtente;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public TipoUtente getTipoUtente() {
+		return tipoUtente;
+	}
+
+	public void setTipoUtente(TipoUtente tipoUtente) {
+		this.tipoUtente = tipoUtente;
+	}
 	
 	
 }
